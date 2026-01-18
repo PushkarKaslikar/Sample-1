159b59d04ec0b36e68259677b2baec955a8f20b7
PushkarKaslikar <166694186+PushkarKaslikar@users.noreply.github.com>
2026-01-18 16:21:26 +0530

    frontend-url-update

--- backend/main.py
+++ backend/main.py
@@ -33,7 +33,7 @@ origins = [
     "http://localhost:3001",
     "http://127.0.0.1:3000",
     "http://127.0.0.1:3001",
-    "https://mech-frontend.vercel.app/"
+    "https://mech-frontend.vercel.app"
 ]
 
 app.add_middleware(
